1c8162efa860daba4a30d760ae30d502d8cc4e93
ArtinMoghadasi <122741147+ArtinMoghadasi@users.noreply.github.com>
2023-01-22 10:11:40 +0330

    Add files via upload

---
+++ NumGuessingGame.cpp
@@ -0,0 +1,48 @@
+/*By Artin Moghadasi
+@Artin.Projects
+*/
+#include <iostream>
+#include <string.h>
+#include <time.h>
+using namespace std;
+int main()
+{
+    cout<<"WELCOME TO NUMBER GUESSING GAME!"<<endl;
+    while (true)
+    {
+        int guess,num,tries;
+        string choice;
+        num = (rand()%100)+1;
+        srand(time(0));
+        do
+        {
+            cout<< "Guess:";
+            cin>>guess;
+            if(guess>num)
+            {
+                cout<<"NUMBER IS LESS"<<endl;
+                continue;
+            }
+            else if(guess<num)
+            {
+                cout<<"NUMBER IS BIGGER" << endl;
+                continue;
+            }
+            else
+            {
+                cout<<"CORRECT!"<<endl;
+                break;
+            }
+        }
+        while(num != guess);
+        cout<<"WANT TO PLAY AGAIN?(yes/no)"<<endl;
+        cin>>choice;
+        if(choice=="YES" || "yes")
+            continue;
+        else
+        {
+            cout<<"BYE!";
+            break;
+        }
+    }
+}
